118ee635b3deb8d2817e57c0374c60a7d87abf22
Tomaz Canabrava <tcanabrava@kde.org>
2026-01-29 13:17:46 +0100

    better animation, crouch controls

--- godot-4.5/scripts/konqui_2d.gd
+++ godot-4.5/scripts/konqui_2d.gd
@@ -4,22 +4,32 @@ class_name Konqui2D
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
+const JUMP_STRENGTH: float = 400.0
+const WALKING_SPEED: float = 300.0
+const RUNNING_SPEED: float = 200.0
+
 var GRAVITY: float = ProjectSettings.get_setting("physics/2d/default_gravity")
-var JUMP_STRENGTH: float = 800.0
-var WALKING_SPEED: float = 300.0
-var RUNNING_SPEED: float = 200.0
-var LIFE: int = 1
+var m_life: int = 1
+var m_is_crouching: bool = false
+var m_is_getting_up: bool = false
+var m_is_crouched: bool = false
+var m_flip_sprite: bool = false
 
 func calc_horizontal_velocity(delta: float) -> float:
-	var input_direction: float = Input.get_axis("left", "right")
-	var is_running: bool = Input.is_action_pressed("run")
-
 	if not is_on_floor():
 		return velocity.x
 
+	if m_is_crouched:
+		return  float(lerp(velocity.x, 0.0, 6 * delta))
+		
+	var input_direction: float = Input.get_axis("left", "right")
+	var is_running: bool = Input.is_action_pressed("run")
+	
 	if is_zero_approx(input_direction):
-		return  float(lerp(velocity.x, 0.0, 10 * delta))
+		return  float(lerp(velocity.x, 0.0, 5 * delta))
 
+	m_flip_sprite = input_direction < 0.0
+	
 	var next_velocity = WALKING_SPEED
 	if is_running:
 		next_velocity += RUNNING_SPEED
@@ -30,14 +40,29 @@ func calc_horizontal_velocity(delta: float) -> float:
 	return next_velocity
 
 func apply_animation() -> void:
-	if LIFE == 0:
+	if m_life == 0:
 		if sprite.animation == "death":
 			return
 		sprite.play("death")
 		return
 	
 	if not is_zero_approx(velocity.x):
-		sprite.flip_h = velocity.x < 0
+		sprite.flip_h = m_flip_sprite
+
+	if m_is_crouching:
+		sprite.stop()
+		sprite.play("crouch")
+		return
+		
+	if m_is_crouched:
+		return
+
+	if m_is_getting_up:
+		sprite.play_backwards("crouch")
+		return
+
+	if sprite.is_playing() && sprite.animation == "crouch":
+		return
 
 	if  not is_on_floor():
 		if Input.is_action_pressed("jump"): # Jump just started
@@ -50,28 +75,50 @@ func apply_animation() -> void:
 		return
 
 	if abs(velocity.x) > 0.0:
-		if sprite.animation != "walk":
+		if sprite.animation != "walk" || !sprite.is_playing():
 			sprite.play("walk")	
 		return
 
-	if is_zero_approx(velocity.x):
+	if is_zero_approx(velocity.x) && sprite.animation == "walk": 
 		sprite.stop() 
 		
 	return
 
-func _physics_process(delta: float) -> void:
-	var is_dying: bool = Input.is_action_just_pressed("fakedeath")
-	if is_dying:
-		LIFE=0
-		apply_animation()
-		return
-		
+func handle_jumping(delta: float) -> bool:
 	var is_jumping: bool = Input.is_action_just_pressed("jump")
 	
 	if is_on_floor() and is_jumping:
 		velocity.y = -JUMP_STRENGTH
 
 	velocity.y += delta * GRAVITY
+	return is_jumping
+
+func handle_death() -> bool:
+	var is_dying: bool = Input.is_action_just_pressed("fakedeath")
+	if is_dying:
+		m_life=0
+		apply_animation()
+		set_physics_process(false)
+	return is_dying
+ 
+func handle_crouching() -> bool:
+	m_is_crouching = Input.is_action_just_pressed("down")
+	m_is_getting_up = Input.is_action_just_released("down")
+	if !m_is_crouching && !m_is_getting_up:
+		return false
+		
+	m_is_crouched = m_is_crouching || m_is_crouched
+	if m_is_getting_up:
+		m_is_crouched = false
+
+	return true
+	
+func _physics_process(delta: float) -> void:
+	if handle_death():
+		return
+
+	handle_jumping(delta)
+	handle_crouching()
 	velocity.x = calc_horizontal_velocity(delta)
 	apply_animation()
 	move_and_slide()
