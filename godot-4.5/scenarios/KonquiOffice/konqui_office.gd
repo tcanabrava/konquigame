62296fe5773f427b0b6572d49b01d5bfc57aeec9
Tomaz Canabrava <tcanabrava@kde.org>
2026-06-03 19:49:37 +0200

    Add a text system, and create a simple text to test things

--- godot-4.5/scenarios/KonquiOffice/konqui_office.gd
+++ godot-4.5/scenarios/KonquiOffice/konqui_office.gd
@@ -6,10 +6,18 @@ class_name konqui_office
 @onready var canvas_modulate: CanvasModulate = $CanvasModulate
 @onready var konqui: Konqui2D = $Konqui
 
-const FlashlightItem = preload("uid://oc0lg1186ma8")
+const FlashlightItemScene = preload("uid://oc0lg1186ma8")
+const DialogueBalloonScene = preload("uid://qdywlyykljc6")
+
+func _on_dialogue_ended(resource: DialogueResource):
+	if resource != DialogueBalloonScene:
+		return
+
+	canvas_modulate.color = "#111111"
 
 func turn_off_light():
-	canvas_modulate.color = "#111111"
+	DialogueManager.show_dialogue_balloon_scene("uid://djfjgwt1guy5f", DialogueBalloonScene, "start")
+	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
 
 func turn_on_light():
 	canvas_modulate.color = "#ffffff"
@@ -18,7 +26,7 @@ func _ready() -> void:
 	turn_on_light()
 	lights_out_timer.timeout.connect(turn_off_light)
 	lights_out_timer.start(3)
-	
-	var flashlight = FlashlightItem.new()
+
+	var flashlight = FlashlightItemScene.new()
 	konqui.add_child(flashlight)
 	konqui.set_current_equipment(flashlight)
